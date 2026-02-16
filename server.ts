881eb05a0dccfbe30dce9a927d4973e39ec513f7
Kelvin Kariuki <kelvinnjuguna357@gmail.com>
2026-02-16 15:38:34 +0300

    initial backend commit

---
+++ server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
